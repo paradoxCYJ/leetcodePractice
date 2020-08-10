148e68a8f28cb8fedf28d60ce926cd47d70cb0ae
paradoxCYJ <mikiru1995chen@gmail.com>
2020-08-10 14:24:08 +0800

    Easy-1290

---
+++ Easy/1290_Convert Binary Number in a Linked List to Integer.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+int getDecimalValue(struct ListNode* head){
+    int result = 0;
+    
+    while( head != NULL ){
+        result <<= 1;
+        result += head->val;
+        head = head->next;
+    }
+    
+    return result;
+}
