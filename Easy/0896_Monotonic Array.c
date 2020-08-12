173342446a9d1a8c38f10898e85309a7b47b9d51
paradoxCYJ <mikiru1995chen@gmail.com>
2020-08-12 12:49:12 +0800

    Easy-0896

---
+++ Easy/0896_Monotonic Array.c
@@ -0,0 +1,25 @@
+bool isMonotonic(int* A, int ASize){
+    if (ASize == 1) {
+        return true;
+    }
+    
+    int prev = A[0];
+    bool is_asc = false;
+    bool is_des = false;
+    for (int i = 1; i < ASize; ++i) {
+        if (A[i] == prev) {
+            continue;
+        }
+        if (A[i] > prev) {
+            is_asc = true;
+        } else {
+            is_des = true;
+        }
+        prev = A[i];
+        
+        if (is_asc && is_des) {
+            return false;
+        }
+    }
+    return true;
+}
